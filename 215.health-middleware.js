b783f284fff97bd06748b66c9adca9948a3fdeff
Jacob Ebey <jacob.ebey@live.com>
2020-05-26 17:19:49 -0700

    Updates

---
+++ 215.health-middleware.js
@@ -0,0 +1,18 @@
+exports.id = 215;
+exports.ids = [215];
+exports.modules = {
+
+/***/ 215:
+/***/ ((module) => {
+
+function ping(req, res) {
+  res.status(200).json("pong");
+}
+
+module.exports = ping;
+
+
+/***/ })
+
+};
+;
